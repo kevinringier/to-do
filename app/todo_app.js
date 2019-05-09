374ffae5518c5cdf95568948a8847a208e2fe120
kevin ringier <kevinringier@gmail.com>
2019-05-08 20:38:38 -0600

    basic functionality

--- app/todo_app.js
+++ app/todo_app.js
@@ -1,6 +1,12 @@
 
 let taskId = 0;
 const TASKS_DIV_ID = "tasks";
+const ADD_TASK = "add_task";
+const ALL_TASKS = "all_tasks";
+const ACTIVE_TASKS = "active_tasks";
+const COMPLETED_TASKS = "completed_tasks";
+const CLEAR_COMPLETED_TASKS = "clear_completed"
+let currentFilter = ALL_TASKS;
 
 class Task {
     constructor(taskName) {
@@ -30,47 +36,75 @@ let getCompleteTasks = function() {
     return tasks.filter(task => task.isComplete == true);
 }
 
-function createTaskElements(taskArray, divId) {
+function clearCompleteTasks() {
+    removeElementsFromDiv(TASKS_DIV_ID);
+    tasks = tasks.filter(task => task.isComplete == false);
+    createTaskElements(filterTasks(currentFilter), TASKS_DIV_ID);
+}
+
+function filterTasks(filter) {
+    switch(filter) {
+        case ALL_TASKS:
+            return tasks;
+        case ACTIVE_TASKS:
+            return getActiveTasks();
+        case COMPLETED_TASKS:
+            return getCompleteTasks();
+    }
+}
+
+function createTaskElements(taskArray, parentElementId) {
     taskArray.forEach(element => {
         taskElement = document.createElement("P");
         taskElement.innerText = element.taskName;
         taskElement.id = element.taskId ;
-        document.getElementById(divId).appendChild(taskElement);
+        document.getElementById(parentElementId).appendChild(taskElement);
     });
 }
 
-function removeElementsFromDiv(divId) {
-    var parentDiv = document.getElementById(divId);
+function removeElementsFromDiv(parentElementId) {
+    var parentDiv = document.getElementById(parentElementId);
     
     while(parentDiv.firstChild) {
         parentDiv.removeChild(parentDiv.firstChild);
     }
 }
 
-// testing stuff
-document.getElementById("task_Form_Btn").addEventListener('click', function() {
+document.getElementById(ADD_TASK).addEventListener('click', function() {
     let taskForm = document.forms["taskForm"]
     let taskName = taskForm.elements["taskInput"].value;
     
     createTask(taskName);
     removeElementsFromDiv(TASKS_DIV_ID);
-    createTaskElements(tasks, TASKS_DIV_ID);
+    createTaskElements(filterTasks(currentFilter), TASKS_DIV_ID);
 });
 
-document.getElementById("all_btn").addEventListener('click', function() {
+document.getElementById(ALL_TASKS).addEventListener('click', function() {
     removeElementsFromDiv(TASKS_DIV_ID);
     createTaskElements(tasks, TASKS_DIV_ID);
+    currentFilter = ALL_TASKS;
 });
 
-document.getElementById("active_btn").addEventListener('click', function() {
+document.getElementById(ACTIVE_TASKS).addEventListener('click', function() {
     removeElementsFromDiv(TASKS_DIV_ID);
     createTaskElements(getActiveTasks(), TASKS_DIV_ID);
+    currentFilter = ACTIVE_TASKS;
 });
 
-document.getElementById("completed_btn").addEventListener('click', function() {
+document.getElementById(COMPLETED_TASKS).addEventListener('click', function() {
     removeElementsFromDiv(TASKS_DIV_ID);
     createTaskElements(getCompleteTasks(), TASKS_DIV_ID);
+    currentFilter = COMPLETED_TASKS;
 });
 
+document.getElementById(TASKS_DIV_ID).addEventListener('click', function(event) {
+    if (event.target && event.target.nodeName == 'P') {
+        tasks.find(element => element.taskId == event.target.id).setIsComplete();
+        removeElementsFromDiv(TASKS_DIV_ID);
+        createTaskElements(filterTasks(currentFilter), TASKS_DIV_ID);
+    }
+});
 
- 
+document.getElementById(CLEAR_COMPLETED_TASKS).addEventListener('click', function() {
+    clearCompleteTasks();
+});
